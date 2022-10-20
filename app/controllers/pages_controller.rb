da345f060506b4fd0ae7a23e305dd727a0198fa0
Sy Rashid <sy.rashid.11@gmail.com>
2022-10-20 17:04:05 +0200

    FEAT: Implement search capability

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def about
     @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
